99612c7b90ecaf52da6aecc6228a19bdd3c75592
TracyDzq <475018651@qq.com>
2016-12-07 22:45:49 +0800

    第五大道项目5个页面完整

---
+++ 5lux/js/login.js
@@ -0,0 +1,56 @@
+$(function(){
+	//加载foot
+	$('#footer').load('footer.html');
+	
+	var register = {
+		regItem:$('.reg-item'),
+		errorInfo:$('.error-info'),
+		user:$('.user'),
+		psw:$('.psw'),
+		sub:$('.reg-sub input'),
+		agree:$('.agree'),
+		reg0:/^1[3|4|5|7|8]\d{9}$/,
+		reg1:/^\w{6,16}$/,
+		init:function(){			
+			this.focus();
+			this.click();
+		},
+		focus:function(){
+			var that=this;
+			this.regItem.focus(function(){
+				$(this).css('border-color',"#269ABC");
+				that.errorInfo.hide();
+			}).blur(function(){
+				$(this).css('border-color',"#ccc");
+			})
+		},
+		click: function(){
+			var that = this;
+			this.sub.click(function(){
+				if( $.trim(that.user.val()).length == 0 ){
+					that.errorInfo.eq(0).show();
+					return;
+				};
+				if( $.trim(that.psw.val()).length == 0 ){
+					that.errorInfo.eq(1).html('密码不能为空！');
+					that.errorInfo.eq(1).show();
+					return;
+				};
+				if( !that.reg0.test( $.trim(that.user.val())) ){
+					that.errorInfo.eq(1).html('用户名或者密码不正确！');
+					that.errorInfo.eq(1).show();
+					return;
+				};
+				if( !that.reg1.test( $.trim(that.psw.val())) ){
+					that.errorInfo.eq(1).html('用户名或者密码不正确！');
+					that.errorInfo.eq(1).show();
+					return;
+				};
+				
+				alert('登录成功');
+			});
+		}
+	};
+	
+	register.init();
+});
---
+++ 5lux/js/register.js
@@ -0,0 +1,59 @@
+$(function(){
+	//加载foot
+	$('#footer').load('footer.html');
+	
+	var register = {
+		regItem:$('.reg-item'),
+		errorInfo:$('.error-info'),
+		mobile:$('.mobile'),
+		code:$('.code'),
+		psw:$('.psw'),
+		conpsw:$('.conpsw'),
+		sub:$('.reg-sub input'),
+		agree:$('.agree'),
+		reg0:/^1[3|4|5|7|8]\d{9}$/,
+		reg1:/^\d{6}$/,
+		reg2:/^\w{6,16}$/,
+		init:function(){			
+			this.focus();
+			this.click();
+		},
+		focus:function(){
+			var that=this;
+			this.regItem.focus(function(){
+				$(this).css('border-color',"#269ABC");
+				that.errorInfo.hide();
+			}).blur(function(){
+				$(this).css('border-color',"#ccc");
+			})
+		},
+		click: function(){
+			var that = this;
+			this.sub.click(function(){
+				if( !that.reg0.test( $.trim(that.mobile.val())) ){
+					that.errorInfo.eq(0).show();
+					return;
+				};
+				if( !that.reg1.test( $.trim(that.code.val())) ){
+					that.errorInfo.eq(1).show();
+					return;
+				};
+				if( !that.reg2.test( $.trim(that.psw.val())) ){
+					that.errorInfo.eq(2).show();
+					return;
+				};
+				if( $.trim( that.psw.val() ) != $.trim( that.conpsw.val() ) ){
+					that.errorInfo.eq(3).show();
+					return;
+				};
+				if( !that.agree.prop('checked') ){
+					alert('请同意条款！');
+					return;
+				}
+				alert('注册成功');
+			});
+		}
+	};
+	
+	register.init();
+});
